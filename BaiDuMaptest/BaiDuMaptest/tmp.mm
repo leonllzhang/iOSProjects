59625d0a31520faf877a99a3b85059532fc7c0fe
LeonLLZhang <lizhang.gis@hotmail.com>
2015-04-14 23:21:55 +0800

    20150414

--- BaiDuMaptest/BaiDuMaptest/tmp.mm
+++
@@ -1,9 +0,0 @@
-//
-//  tmp.m
-//  BaiDuMaptest
-//
-//  Created by Leon Zhang on 15/4/8.
-//  Copyright (c) 2015年 Leon Zhang. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
--- BaiMapTest2/BaiMapTest2/test.mm
+++
@@ -1,9 +0,0 @@
-//
-//  test.m
-//  BaiMapTest2
-//
-//  Created by Leon Zhang on 15/4/8.
-//  Copyright (c) 2015年 Leon Zhang. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
